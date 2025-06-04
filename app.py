4e32f84db126029e65597777b99a3f950e804e3a
XinzhiJiang <amy.jxz@gmail.com>
2025-06-04 21:49:59 +0200

    Updated UI and calculation in page 1 and 2
    1. CO2 data tabel toegevoegd in pagina 1.
    2. Parallel coord plot in pagina 2 geupdatet, met 4 kolommen: MKI, kost en CO2 als ambities, en index van opties.
    
    Note: df_options is voor nu voor alle materiaal combinaties; WWR is nog niet meegenomen in calculatie.

--- app.py
+++ app.py
@@ -75,6 +75,7 @@ import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 import numpy as np
 import pandas as pd
+from itertools import product
 
 
 class Parametrization(vkt.Parametrization):
@@ -89,7 +90,7 @@ class Parametrization(vkt.Parametrization):
     _mki_table_default = [
         {"category": "Facade - open", "material": "Double glazing", "mki_per_unit": 50, "unit": "m² open", "nmd_code": "#nmd_00000"},
         {"category": "Facade - closed", "material": "Prefab concrete sandwich panels", "mki_per_unit": 10, "unit": "m² closed", "nmd_code": "#nmd_00000"},
-        {"category": "Facade - closed", "material": "Steel sandwich panels", "mki_per_unit": 10, "unit": "m² closed", "nmd_code": "#nmd_00000"},
+        {"category": "Facade - closed", "material": "Steel sandwich panels", "mki_per_unit": 11, "unit": "m² closed", "nmd_code": "#nmd_00000"},
         {"category": "Facade - closed", "material": "Timber frame construction (HSB)", "mki_per_unit": 5, "unit": "m² closed", "nmd_code": "#nmd_00000"}
     ]
     _mki_table_category_options = ["Facade - open", "Facade - closed"]
@@ -118,6 +119,23 @@ class Parametrization(vkt.Parametrization):
     facade_size.data.cost_table.cost_per_unit = vkt.NumberField("Initial cost [€/unit]")
     facade_size.data.cost_table.unit = vkt.TextField("Unit")
     facade_size.data.cost_table.nmd_code = vkt.TextField("NMD code")
+
+    # user-modifiable table for CO2 data
+    _co2_table_default = [
+        {"category": "Facade - open", "material": "Double glazing", "co2_per_unit": 50, "unit": "m² open", "nmd_code": "#nmd_00000"},
+        {"category": "Facade - closed", "material": "Prefab concrete sandwich panels", "co2_per_unit": 40, "unit": "m² closed", "nmd_code": "#nmd_00000"},
+        {"category": "Facade - closed", "material": "Steel sandwich panels", "co2_per_unit": 35, "unit": "m² closed", "nmd_code": "#nmd_00000"},
+        {"category": "Facade - closed", "material": "Timber frame construction (HSB)", "co2_per_unit": 10, "unit": "m² closed", "nmd_code": "#nmd_00000"}
+    ]
+    _co2_table_category_options = ["Facade - open", "Facade - closed"]
+    facade_size.data.co2_subtitle = vkt.Text("CO2 data")
+    facade_size.data.co2_table = vkt.Table("cCO Table", default=_co2_table_default)
+    facade_size.data.co2_table.category = vkt.OptionField("Category", options=_co2_table_category_options)
+    facade_size.data.co2_table.material = vkt.TextField("Material")
+    facade_size.data.co2_table.co2_per_unit = vkt.NumberField("CO2 [kg/unit]")
+    facade_size.data.co2_table.unit = vkt.TextField("Unit")
+    facade_size.data.co2_table.nmd_code = vkt.TextField("NMD code")
+
 
     result = vkt.Page("2. Overvew all options", views=['plotly_pcp_view'])  # showing the parallel coordinate plot
 
@@ -170,12 +188,6 @@ class Parametrization(vkt.Parametrization):
     configurations.facade_3.params.num_windows = vkt.IntegerField("Number of windows per storey", min=1, max=5, default=2)
 
 
-# @staticmethod
-# def mki_table_default():
-#     return [
-#         {"category": "Window", "material": "Double glazing", "mki": 10, "unit": "m²"},
-#         {"category": "Gevel", "material": "Prefab concrete sandwich panels", "mki": 5, "unit": "m²"}
-#     ]
 
 class Controller(vkt.Controller):
     label = "Facade Configuration"
@@ -188,12 +200,6 @@ class Controller(vkt.Controller):
         'Timber frame construction (HSB)': vkt.Color(210, 180, 140)  # Tan
     }
 
-    # def __init__(self):
-    #     super().__init__()
-    #     self.initial_mki_data = self.parametrization.mki_table_default()
-
-    # def set_initial_mki_data(self, params):
-    #     params.facade_size.data.mki_table = self.initial_mki_data
 
     def calculate_outputs(self, facade_params, facade_size):
 
@@ -201,46 +207,93 @@ class Controller(vkt.Controller):
         print("facade_size.data.mki_table:", facade_size.data.mki_table)
         db_mki_table = facade_size.data.mki_table  # this is a list of Munch elements: [Munch({'category': 'Facade - open', 'material': 'Double glazing', ...
         db_cost_table = facade_size.data.cost_table
+        db_co2_table = facade_size.data.co2_table
 
-        # Example df_options
-        df_options = pd.DataFrame([
-            {'facade_open': 'Double glazing', 'facade_close': 'Prefab concrete sandwich panels'},
-            {'facade_open': 'Double glazing', 'facade_close': 'Timber frame construction (HSB)'}
-        ])
+        # create df_options
+        def sort_unique_facade_materials(db_table, mat_category_str, col_name_str):
+            filtered_materials = [
+                item for item in db_table
+                if getattr(item, 'category', None) == mat_category_str and hasattr(item, col_name_str)
+            ]
+            sorted_materials = sorted(filtered_materials, key=lambda x: getattr(x, col_name_str))
+            return [item.material for item in sorted_materials]
+
+        unique_mat_open_sorted_mki = sort_unique_facade_materials(db_mki_table, "Facade - open", "mki_per_unit")
+        unique_mat_close_sorted_mki = sort_unique_facade_materials(db_mki_table, "Facade - closed", "mki_per_unit")
+        print(unique_mat_open_sorted_mki, unique_mat_close_sorted_mki)
+
+        unique_mat_open_sorted_cost = sort_unique_facade_materials(db_cost_table, "Facade - open", "cost_per_unit")
+        unique_mat_close_sorted_cost = sort_unique_facade_materials(db_cost_table, "Facade - closed", "cost_per_unit")
+        print(unique_mat_open_sorted_cost, unique_mat_close_sorted_cost)
+
+        unique_mat_open_sorted_co2 = sort_unique_facade_materials(db_co2_table, "Facade - open", "co2_per_unit")
+        unique_mat_close_sorted_co2 = sort_unique_facade_materials(db_co2_table, "Facade - closed", "co2_per_unit")
+        print(unique_mat_open_sorted_co2, unique_mat_close_sorted_co2)
+
+        # wwr: ranging from 20 to 80 at interval of 20
+        wwr_option_list = [20, 40, 60, 80]
+
+        # column names of df_option
+        col_name_all = [
+            "facade_open",
+            "facade_close",
+            "wwr",
+        ]
+        allCombo = list(product(unique_mat_open_sorted_mki, unique_mat_close_sorted_mki, wwr_option_list
+                                ))
+        df_options_list = np.array(allCombo).T.tolist()
+        df_options = pd.DataFrame({col_name_all[0]: df_options_list[0]})
+        for i in range(len(col_name_all) - 1):
+            df_options[col_name_all[i + 1]] = df_options_list[i + 1]
+
+        print("df_options:")
+        print(df_options.to_string())
+
+
+        # # Example df_options
+        # df_options = pd.DataFrame([
+        #     {'facade_open': 'Double glazing', 'facade_close': 'Prefab concrete sandwich panels'},
+        #     {'facade_open': 'Double glazing', 'facade_close': 'Timber frame construction (HSB)'}
+        # ])
 
         # --- Quantity Constants ---
-        quantity_facade_open = 10
-        quantity_facade_close = 20
+        area_facade_total = facade_size.dimensions.width * facade_size.dimensions.num_storeys * facade_size.dimensions.storey_height
+        area_facade_open = area_facade_total * 20  # todo: to use wwr
+        area_facade_close = area_facade_total * 80  # todo: to use wwr
 
         # --- Create Lookups ---
         mki_dict = {item.material: item.mki_per_unit for item in db_mki_table}
         cost_dict = {item.material: item.cost_per_unit for item in db_cost_table}
+        co2_dict = {item.material: item.co2_per_unit for item in db_co2_table}
         unit_dict = {item.material: item.unit for item in db_mki_table}  # Prefer MKI unit, assume it's authoritative
 
         # --- Function to decide quantity based on unit ---
         def get_quantity_for_unit(unit: str) -> float:
             if unit == "m² open":
-                return quantity_facade_open
+                return area_facade_open
             elif unit == "m² closed":
-                return quantity_facade_close
+                return area_facade_close
             elif unit == "m² total facade":
-                return quantity_facade_open + quantity_facade_close
+                return area_facade_total
             else:
                 return 2  # Default fallback
 
         # --- Row-wise computation ---
-        def calculate_mki_cost_from_units(row):
+        def calculate_mki_cost_from_units(row):  # todo: use wwr
             total_mki = 0
             total_cost = 0
+            total_co2 = 0
             for col in df_options.columns:
                 material = row[col]
                 unit = unit_dict.get(material, "")
                 quantity = get_quantity_for_unit(unit)
                 mki = mki_dict.get(material, 0)
                 cost = cost_dict.get(material, 0)
+                co2 = co2_dict.get(material, 0)
                 total_mki += mki * quantity
                 total_cost += cost * quantity
-            return pd.Series({'mki': total_mki, 'cost': total_cost})
+                total_co2 += co2 * quantity
+            return pd.Series({'mki': total_mki, 'cost': total_cost, 'co2': total_co2})
 
         # --- Apply calculation ---
         df_options_results = df_options.apply(calculate_mki_cost_from_units, axis=1)
@@ -296,11 +349,20 @@ class Controller(vkt.Controller):
         embedded_co2s = []
         energy_uses = []
 
+
+
+
+
         for facade in facades:
             initial_cost, embedded_co2, energy_use, df_options_results, df_options = self.calculate_outputs(facade, params.facade_size)
             initial_costs.append(initial_cost)
             embedded_co2s.append(embedded_co2)
             energy_uses.append(energy_use)
+
+        print("type df_options_results['mki']:", type(df_options_results['mki']), df_options_results['mki'])
+
+        # add index of options to the plot as a separate column
+        option_idx = pd.Series(range(len(df_options)))
 
         # Create subplots
         fig = go.Figure(data=
@@ -315,6 +377,14 @@ class Controller(vkt.Controller):
                         multiselect=False,
                         label="Cost [€]",
                         values=df_options_results['cost']),
+                    dict(
+                        multiselect=False,
+                        label="CO2 [kg]",
+                        values=df_options_results['co2']),
+                    dict(
+                        multiselect=False,
+                        label="Option",
+                        values=option_idx),
                     # dict(range=[1, 5],
                     #      tickvals=[1, 2, 4, 5],
                     #      label='C', values=[2, 4],
@@ -488,9 +558,6 @@ class Controller(vkt.Controller):
         )
 
         return PlotlyResult(fig.to_json())
-
-
-
 
 
 
